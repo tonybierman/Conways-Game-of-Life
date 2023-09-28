1090703e7691fe0b534cb6025f90d0e866c534b4
Tony Bierman <tonybierman@gmail.com>
2023-09-28 09:27:04 -0500

    Header blocks

--- GameOfLife/App.axaml.cs
+++ GameOfLife/App.axaml.cs
@@ -1,3 +1,20 @@
+/*
+ * Author: Tony Bierman
+ * Website: http://www.tonybierman.com
+ * License: MIT License
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
@@ -19,7 +36,7 @@ public class App : Application
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime lifetime)
         {
             var mainWindow = new MainWindow();
-            var field = new Landscape(Rules.BriansBrainRuleset(), BriansBrainPatterns.GetSmallBurstCoords(1, 1));
+            var field = new Landscape(Rules.GameOfLifeDefault(), GameOfLifePatterns.GetGliderCoords(1, 1));
             var game = new Game(field);
             game.Start();
             mainWindow.DataContext = field;
